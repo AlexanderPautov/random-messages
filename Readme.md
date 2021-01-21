144091ae8ab3fc934dda43cc88097d38bd2e2f12
AlexanderPautov <pautovalexander95@gmail.com>
2021-01-21 18:20:12 +0300

    Added Readme and updated the code in index.js

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Random messages generator
+
+This project is from Codecademy Pro Full-Stack Enginner path. You can run it using Node.js. It randomly generates 3 messages getting some words from an array.
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+const greetings = ['great', 'nice', 'beautiful', 'fun', 'stunning', 'wonderful']
+const doSmth    = ['work harder', 'get better', 'keep working', 'be patient']
+const wishes    = ['Have fun', 'Good luck', 'Good day', 'Keep up the good work']
+
+const randomWord = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)] 
+}
+
+let greet = `Hi! Today is a ${randomWord(greetings)} day!`
+let something = `You should ${randomWord(doSmth)}`
+let wish = `${randomWord(wishes)}!`
+
+console.log(`${greet} \n${something} \n${wish}`)
